143425a86653517a50e16d848819eb92ca38cc01
Tomasz Pakuła <forest10pl@gmail.com>
2023-07-10 23:57:13 +0200

    Little fix on the code. Confirmed working

--- function_app.py
+++ function_app.py
@@ -3,10 +3,10 @@ import logging
 import obtain_secret
 
 app = func.FunctionApp()
-@app.function_name(name="ShowSecret")
-@app.route(route="show-secret", auth_level=func.AuthLevel.ANONYMOUS)
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    
+
+@app.function_name(name="getSecret")
+@app.route(route="getSecret", auth_level=func.AuthLevel.ANONYMOUS)
+def getSecret(req: func.HttpRequest) -> func.HttpResponse:
     secret = obtain_secret.get_secret_value()
     code = 200
 
