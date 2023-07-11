e03c3586ddef7c838489b49f0c951a09cc36bd06
Tomasz Pakuła <forest10pl@gmail.com>
2023-07-11 02:37:48 +0200

    Remove line breaks

--- function_app.py
+++ function_app.py
@@ -13,14 +13,12 @@ def getsecret(req: func.HttpRequest) -> func.HttpResponse:
     res_body = "Application version: "
     with open('application.version', 'r') as file:
         res_body += file.readline()
-        
-    res_body += "<br><br>"
     
     # detect errors
     if secret[0] != None:
         code = 401
     else:
-        res_body += f"<br><br>{str(secret[1])}"
+        res_body += f"{str(secret[1])}"
 
     response = func.HttpResponse(res_body, status_code=code)
     logging.info(f"Processed request. Status: {code}")
